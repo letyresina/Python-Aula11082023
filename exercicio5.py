c34f3171dfcc47106169f634bb22961fdcc545cb
letyresinaa <leticiaresina@gmail.com>
2023-08-11 11:32:13 -0300

    subindo exercício 5

---
+++ exercicio5.py
@@ -0,0 +1,10 @@
+'''
+    Defina uma função chamada concatenar_strings que aceite duas strings e um 
+    parâmetro nomeado separador com um valor padrão de espaço. A função deve 
+    retornar as duas strings concatenadas com o separador entre elas.
+'''
+
+def concatenar_strings(string1, string2, separador = " "):
+    print(string1 + separador + string2)
+
+concatenar_strings("a", "b", "       ")
